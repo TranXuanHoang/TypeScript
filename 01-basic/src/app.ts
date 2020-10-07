7689a9f2b257590adf067fe6c404c84218af1ac9
TranXuanHoang <hoangtx.ict@gmail.com>
2020-10-07 12:45:16 +0900

    Write TypeScript logic to interact with DOM elements

--- 01-basic/src/app.ts
+++ 01-basic/src/app.ts
@@ -1,1 +1,59 @@
-console.log('Your code goes here...!')
+/**
+ * An autobinding decorator that will bind the `this` keyword refering to
+ * the object instance of the decorated method. This is to avoid the need
+ * of calling `method.bind(this)`.
+ *
+ * More information about [method decorators](https://www.typescriptlang.org/docs/handbook/decorators.html#method-decorators).
+ */
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element.id = 'user-input'
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+    this.configure()
+    this.attach()
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault()
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+}
+
+const prjInput = new ProjectInput()
